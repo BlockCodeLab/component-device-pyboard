f60d01e5ad19bbd1a18b8d408ff187c814452312
Yeqin Gong <gon@gyeq.in>
2024-09-23 11:50:51 +0800

    add flash check

--- src/device-util.js
+++ src/device-util.js
@@ -30,27 +30,53 @@ export const disconnectDevice = async (board, setDevice) => {
 export const showDownloadScreen = async (board, displayPackage) => {
   await board.stop();
   await board.enterRawRepl();
-  await board.execRaw(`from ${displayPackage} import display`);
-  await board.execRaw('import popui.download_screen as download_screen');
-  await board.execRaw('download_screen.render(display)');
+  // await board.execRaw(`from ${displayPackage} import display`);
+  // await board.execRaw('import popui.screen.download as download_screen');
+  // await board.execRaw('download_screen.render(display)');
+  let line = '';
+  line += 'import popui.screen.download as download_screen\n';
+  line += `from ${displayPackage} import display\n`;
+  line += 'download_screen.render(display)\n';
+  const out = await board.execRaw(line);
   await board.exitRawRepl();
+  return out;
 };
 
 export const configDevice = async (board, settings) => {
   await board.stop();
   await board.enterRawRepl();
-  await board.execRaw('import device.config as config');
+  // await board.execRaw('import device.config as config');
+  let line = 'import device.config as config\n';
   for (const [key, value] of Object.entries(settings)) {
     if (typeof value === 'number' && value % 1 === 0) {
-      await board.execRaw(`config.set_int("${key}", ${value})`);
+      // await board.execRaw(`config.set_int("${key}", ${value})`);
+      line += `config.set_int("${key}", ${value})\n`;
     } else if (typeof value === 'boolean') {
-      await board.execRaw(`config.set_bool("${key}", ${value ? 'True' : 'False'})`);
+      // await board.execRaw(`config.set_bool("${key}", ${value ? 'True' : 'False'})`);
+      line += `config.set_bool("${key}", ${value ? 'True' : 'False'})\n`;
     } else {
-      await board.execRaw(`config.set_str("${key}", "${value}")`);
+      // await board.execRaw(`config.set_str("${key}", "${value}")`);
+      line += `config.set_str("${key}", "${value}")\n`;
     }
   }
-  await board.execRaw('config.save()');
+  // await board.execRaw('config.save()');
+  line += 'config.save()\n';
+  const out = await board.execRaw(line);
   await board.exitRawRepl();
+  return out;
+};
+
+export const checkFlash = async (board, files) => {
+  await board.stop();
+  let size = 0;
+  for (const file of files) {
+    size += file.content?.length ?? file.data?.length ?? 0;
+  }
+  await board.enterRawRepl();
+  const line = `from device.flash import check_flash_free\nprint(check_flash_free(${size}))\n`;
+  const output = await board.execRaw(line);
+  await board.exitRawRepl();
+  return output.slice(2, -3).trim() === 'True';
 };
 
 export const writeFiles = async (board, files, progress) => {
